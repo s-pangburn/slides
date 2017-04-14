905eaf5c6788cfbac001c84d1f6441bdf325398c
Claire Rogers <clairekrogers@gmail.com>
2017-04-13 19:35:10 -0700

    add presentation progress bar

--- components/presentation.jsx
+++ components/presentation.jsx
@@ -17,20 +17,27 @@ class Presentation extends React.Component {
   }
 
   _toggleSlide(e) {
-    e.preventDefault();
     const currentSlide = this.state.currentSlide;
 
     if(e.key === "ArrowRight" && currentSlide < this.props.slides.length - 1) {
+      e.preventDefault();
       this.setState({ currentSlide: currentSlide + 1 });
     } else if (e.key === "ArrowLeft" && currentSlide > 0) {
+      e.preventDefault();
       this.setState({ currentSlide: currentSlide - 1 });
-    }
+    } 
   }
 
   render() {
+    const progress = Math.round((this.state.currentSlide + 1) / this.props.slides.length * 100);
+    const progressString = `${progress}%`;
+
+   debugger; 
+
     return (
       <div className="present-container">
         <div className="present-slide render-preview" dangerouslySetInnerHTML={this.rawMarkup()}/>
+        <div className="present-progress" style={{ width: progressString }}/>
       </div>
     );
   }
