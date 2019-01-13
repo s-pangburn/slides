c1513ea6ae113d1724d2fe2552240d8f669e0574
Jon Wolverton <wolverton.jr@gmail.com>
2019-01-12 23:42:11 -0800

    parse slides in reducer

--- frontend/reducers/slides_reducer.js
+++ frontend/reducers/slides_reducer.js
@@ -2,12 +2,25 @@ import {
   RECEIVE_STATE
 } from '../actions';
 
+const SLIDE_DELIMITER = /[^-]---[^-]/g;
+const NOTE_DELIMITER = "\nNote:";
+
+function parseSlides(text) {
+  return text.split(SLIDE_DELIMITER).map(slideText => {
+    const parts = slideText.split(NOTE_DELIMITER);
+    return {
+      markdown: parts[0],
+      notes: parts.slice(1).join(NOTE_DELIMITER)
+    };
+  });
+}
+
 const slidesReducer = (state = [], action) => {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_STATE:
-      return [];
+      return parseSlides(action.text);
     default:
       return state;
   }
