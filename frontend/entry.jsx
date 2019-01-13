ba6cab27a9bf490887e2f263d6a1c182186c392e
Jon Wolverton <wolverton.jr@gmail.com>
2019-01-13 00:08:04 -0800

    setup entry file & get webpack working

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Edit from './edit';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
+import configureStore from './store/store';
+import * as actions from './actions';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <HashRouter>
+      <Route path="/" component={() => (<div>HELLO!</div>)} />
+    </HashRouter>
+  </Provider>
+);
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.store = store;
+  window.actions = actions;
   const root = document.getElementById("root");
-  ReactDOM.render(
-    <Router>
-      <Route path="/" component={Edit} />
-    </Router>, root);
+
+  ReactDOM.render(<Root store={store} />, root);
 });
